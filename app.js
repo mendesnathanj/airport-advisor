355fa68534b301246eca2ade4ffd9555986a4a5c
Dilan Balci <dlanbalci@gmail.com>
2020-03-18 11:01:16 -0700

    Fixed login bugs.

--- app.js
+++ app.js
@@ -17,3 +17,12 @@ app.get("/", (req, res) => res.send("hewwo world!!"));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const users = require("./routes/api/users");
+app.use("/api/users", users);
+
+
+const passport = require('passport');
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
