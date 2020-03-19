a43dfeb94649eac0698a3020a3f278fd81b7f20a
terrychow <terryhchow@gmail.com>
2020-03-19 11:03:31 -0700

    create search results component

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.scss';
 import Modal from './modal/modal'
 import ModalTest from './modal/modal_test';
+import AirportResults from './airport_results/airport_results'
 
 const App = () => (
   <div className="app">
-    hewwo there!!!
     <header>
       <ModalTest/>
     </header>
       <Modal />
+      <AirportResults/>
   </div>
 );
 
