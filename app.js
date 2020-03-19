7479faf9c2e5f612bcd4afbe3920b87d97a8043c
Dilan Balci <dlanbalci@gmail.com>
2020-03-19 14:05:28 -0700

    Token key added user object for signup problem.

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("hewwo world!!"));
-
+app.use(express.static('frontend/build'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+})
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
