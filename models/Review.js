7d781ba07be145b5fd898042d6312b58c8e27ca4
Dilan Balci <dlanbalci@gmail.com>
2020-03-20 11:22:47 -0700

    Review routes fixed.

--- models/Review.js
+++ models/Review.js
@@ -38,6 +38,7 @@ const ReviewSchema = new Schema({
         },
         general_score: {
             type: Number,
+            required: true
         }
     },
     date: {
--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -2,40 +2,18 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+const ObjectId = mongoose.Types.ObjectId;
 
 const Review = require('../../models/Review');
 const Airport = require('../../models/Airport');
 const validateReviewInput = require('../../validations/reviews');
 
-
-//index page
-    //which we dont need i think
 router.get('/', (req, res) => {
     Review.find()
         .sort({ date: -1 })
         .then(reviews => res.json(reviews))
         .catch(err => res.status(404).json({ noreviewsfound: 'No reviews found' }));
 });
-
-// router.get('/user/:user_id', (req, res) => {
-//     Review.find({ user: req.params.user_id })
-//         .sort({ date: -1 })
-//         .then(reviews => res.json(reviews))
-//         .catch(err =>
-//             res.status(404).json({ noreviewsfound: 'No reviews found from that user' }
-//             )
-//         );
-// });
-
-// router.get('/airport/:airport_id', (req, res) => {
-//     Review.find({ airport: req.params.airport_id })
-//         .sort({ date: -1 })
-//         .then(reviews => res.json(reviews))
-//         .catch(err =>
-//             res.status(404).json({ noreviewsfound: 'No reviews found from that airport' }
-//             )
-//         );
-// });
 
 router.get('/:id', (req, res) => {
     Review.findById(req.params.id)
@@ -46,7 +24,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    Review.deleteOne({ id: req.params.id })
+    Review.findByIdAndDelete(req.params.id)
         .then(() => {
             res.json({ staus: "OK" })
         })
@@ -55,28 +33,10 @@ router.delete('/:id', (req, res) => {
         );
 });
 
-router.put('/:id', (req,res) => {
-    Review.updateOne(
-        { id: req.params.id }, 
-        { set: {
-            review: req.body.review,
-            airport: req.body.airport_id,
-            ratings: {
-                transportation: req.body.ratings.transportation,
-                restaurants: req.body.ratings.restaurants,
-                waiting_hall: req.body.ratings.waiting_hall,
-                wifi_charging: req.body.ratings.wifi_charging,
-                sleepability: req.body.ratings.sleepability,
-                cleanliness: req.body.ratings.cleanliness,
-                security: req.body.ratings.security,
-                general_score: req.body.ratings.general_score
-            }}})
-            .then(() => {
-                res.json({ success: true })
-            })
-            .catch(err =>
-                res.status(404).json({ noreviewfound: 'No review found with that ID' })
-            );
+router.patch('/:id', (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(node => res.json(node))
+        .catch(err => res.status(404).json(err))
 });
 
 router.post('/',
