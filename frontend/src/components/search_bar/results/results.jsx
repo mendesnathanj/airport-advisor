700007ef09cb70643c98afde31731d9b568d8633
Nathan <mendesnathanj@gmail.com>
2020-03-22 15:49:41 -0700

    Fix result items from not disappearing when user clicks on link

--- frontend/src/components/search_bar/results/results.jsx
+++ frontend/src/components/search_bar/results/results.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import "./Results.scss";
 import ResultItem from './result_item';
 
-const Results = ({ searchItems }) => {
+const Results = ({ handleClear, searchItems }) => {
   if (searchItems.length === 0) return null;
 
   const resultItems = searchItems
                         .map((airport, i) =>
-                          <ResultItem key={i} airport={airport} />
+                          <ResultItem handleClick={handleClear} key={i} airport={airport} />
                         );
 
   return <div className="search-results">{resultItems}</div>;
