a8100b8b011a5733320f5e2cac776ece75d69792
terrychow <terryhchow@gmail.com>
2020-03-22 11:40:26 -0700

    show page styling, link write a review button

--- frontend/src/components/airport-show/show_form.js
+++ frontend/src/components/airport-show/show_form.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './show.scss'
 import RatingItem from './rating_item';
 import ShowRating from './show-ratings/ratings'
+import RatingContainer from '../airport_results/rating_container/rating_container'
 
 class ShowForm extends React.Component {    
     componentDidMount() {
@@ -16,33 +17,62 @@ class ShowForm extends React.Component {
               <h1>
                 {airport.name}, {airport.code}
               </h1>
-              <h1>
-                {airport.city}, {airport.country}
-              </h1>
-              <h1>Overall Rating</h1>
-              <h1>{airport.avg_score.general_score}</h1>
-              <h1>{airport.review_count} ratings</h1>
-              <h1>Airport Individual Ratings</h1>
+              <span className="rating-average">
+                <h1 className="main-score">
+                  <RatingContainer num={airport.avg_score.general_score} />
+                </h1>
+                <h1 className="main-score">{airport.review_count} ratings</h1>
+              </span>
               <div className="optional-reviews">
-                <ul>
-                  <li>{airport.avg_score.transportation}</li>
-                  <li>{airport.avg_score.restaurants}</li>
-                  <li>{airport.avg_score.waiting_hall}</li>
-                  <li>{airport.avg_score.wifi_charging}</li>
-                  <li>{airport.avg_score.sleepability}</li>
-                  <li>{airport.avg_score.cleanliness}</li>
-                  <li>{airport.avg_score.security}</li>
+                <ul className="op-reviews">
+                  <span>
+                    Transportation{" "}
+                    <RatingContainer num={airport.avg_score.transportation} />
+                  </span>
+                  <span>
+                    Restaurants{" "}
+                    <RatingContainer num={airport.avg_score.restaurants} />
+                  </span>
+                  <span>
+                    Waiting Hall{" "}
+                    <RatingContainer num={airport.avg_score.waiting_hall} />
+                  </span>
+                  <span>
+                    Wifi Charging{" "}
+                    <RatingContainer num={airport.avg_score.wifi_charging} />
+                  </span>
+                </ul>
+                <ul className="op-reviews">
+                  <span>
+                    Sleepability{" "}
+                    <RatingContainer num={airport.avg_score.sleepability} />
+                  </span>
+                  <span>
+                    Cleanliness{" "}
+                    <RatingContainer num={airport.avg_score.cleanliness} />
+                  </span>
+                  <span>
+                    Security{" "}
+                    <RatingContainer num={airport.avg_score.security} />
+                  </span>
                 </ul>
               </div>
-              <button className="review-btn">Write a Review</button>
+              {!!this.props.currentUser ? (
+                <button
+                  className="review-btn"
+                  onClick={() => this.props.openModal("new-review")}
+                >
+                  Write a Review
+                </button>
+              ) : null}
             </header>
             <br></br>
             <div className="show-body">
-              <ul>
+              <div className="show-body-child">
                 {airport.reviews.map(review => (
                   <RatingItem review={review} key={review._id} />
                 ))}
-              </ul>
+              </div>
             </div>
           </div>
         );
